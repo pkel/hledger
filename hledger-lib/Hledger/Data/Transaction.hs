2fe7fc22b6a889864636672aa168ba75d99e23c4
Stephen Morgan <morgan.stephen99@gmail.com>
2021-08-08 18:03:46 +1000

    dev: lens: Introduce lenses for InputOpts and BalancingOpts.

--- hledger-lib/Hledger/Data/Transaction.hs
+++ hledger-lib/Hledger/Data/Transaction.hs
@@ -9,11 +9,12 @@ tags.
 
 {-# LANGUAGE FlexibleContexts  #-}
 {-# LANGUAGE LambdaCase        #-}
+{-# LANGUAGE NamedFieldPuns    #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE Rank2Types        #-}
 {-# LANGUAGE RecordWildCards   #-}
+{-# LANGUAGE TemplateHaskell   #-}
 
-{-# LANGUAGE NamedFieldPuns #-}
 module Hledger.Data.Transaction (
   -- * Transaction
   nulltransaction,
@@ -30,6 +31,7 @@ module Hledger.Data.Transaction (
   balancedVirtualPostings,
   transactionsPostings,
   BalancingOpts(..),
+  HasBalancingOpts(..),
   defbalancingopts,
   isTransactionBalanced,
   balanceTransaction,
@@ -77,6 +79,7 @@ import qualified Data.Map as M
 import Safe (maximumDef)
 
 import Hledger.Utils
+import Hledger.Utils.TH (makeHledgerClassyLenses)
 import Hledger.Data.Types
 import Hledger.Data.Dates
 import Hledger.Data.Posting
@@ -659,6 +662,10 @@ transactionFile Transaction{tsourcepos} =
   case tsourcepos of
     GenericSourcePos f _ _ -> f
     JournalSourcePos f _   -> f
+
+-- lenses
+
+makeHledgerClassyLenses ''BalancingOpts
 
 -- tests
 
